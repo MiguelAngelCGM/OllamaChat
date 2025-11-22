81dc363c4d59559d187fb03ae5f778d7959321fe
Miguel Ángel Cueto Gómez-Morán <yosoysoloyo1991@gmail.com>
2025-11-22 11:43:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description="Simple chatbot using Ollama",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/AIShinyStardust/OllamaChat",
+    url="https://github.com/MiguelAngelCGM/OllamaChat",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
